9b35a1187425f7decedd53ed3df713b3eceacf4a
maicol torres <maicolt401@gmail.com>
2022-05-17 01:37:22 +0200

    elaborate styled to components

--- src/components/RobotList/RobotList.js
+++ src/components/RobotList/RobotList.js
@@ -6,21 +6,26 @@ const RobotList = (index, robot) => {
   const robots = useSelector((state) => state.robots);
 
   const H1 = styled.h1`
+    background-color: black;
     color: white;
+    border-radius: 25px;
     display: flex;
     justify-content: center;
+  `;
+  const Ul = styled.ul`
+    padding-left: 0px;
   `;
 
   return (
     <>
       <H1>My Robotpedia</H1>
-      <ul>
+      <Ul>
         {robots.map((robot) => (
           <li>
             <Robot key={robots._id} robot={robot} />
           </li>
         ))}
-      </ul>
+      </Ul>
     </>
   );
 };
